00a8ac4788595d905ebeba8e7b2ae6c3b882337d
Eashan Mathur <eashanmathur@gmail.com>
2022-07-21 21:55:34 -0700

    Temp site

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div style = {{marginTop: 200, textAlign: 'center'}}>
+      <h1 style = {{fontSize: 80, fontFamily:'Syne'}}>Hi, I'm <br/> Eashan Mathur</h1>
+      <p style = {{fontSize: 30, fontFamily: 'Manrope'}}>Senior at the University of California, Berkeley. <br/>iOS Developer and Designer.</p>
+      <p style = {{marginTop: 80,fontSize: 30, fontFamily: 'Manrope'}}>Website in development.</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/experiencesBox.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React from 'react';
+
+function experienceBox(props) {
+    const isLargerBox = props.isLargerBox;
+    
+    if (isLargerBox) {
+        return (
+            <div>
+                
+            </div>
+        ) 
+    } else {
+
+    }
+}
+
+export default experienceBox;
